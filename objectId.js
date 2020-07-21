691521e6321194aeb1b86b450ce295e7200dbaf2
anoop-codes <kumar.anoop.9826@gmail.com>
2020-07-21 10:20:39 +0530

    movies and rentals

---
+++ objectId.js
@@ -0,0 +1,14 @@
+/**
+ * objectid : consist of 12 byte
+ *
+ * first 4 byte repsent time stamp -- time of creation > id.getTimestamp();
+ *
+ * next 3 byte : machine identifer
+ *
+ * next 2 byte : diff process
+ *
+ * next 3 byte: counter
+ * 
+ * 
+ * 
+ */
