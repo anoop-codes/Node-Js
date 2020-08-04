60f109aa974180d0e43a3c251db0025c15336c55
anoop-codes <kumar.anoop.9826@gmail.com>
2020-08-04 09:37:06 +0530

    refactoring

--- startup/logging.js
+++ startup/logging.js
@@ -2,10 +2,10 @@ module.exports = () => {
 
     process.on('unhandledRejection', (error) => {
         console.log('UNHANDLED REJECTION :', error);
-        process.exit(1);
     });
 
     process.on('uncaughtException', (error) => {
         console.log('UNCAUGHT EXCEPETION :', error.message)
+        process.exit(1);
     });
 }
